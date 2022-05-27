48762681674ed99746ff854eb34602a8a3887521
pedrobits <pedroksk2@gmail.com>
2022-05-27 12:16:34 -0300

    Inclusão e formatação da mensagem.

--- botWhatsapp.py
+++ botWhatsapp.py
@@ -17,7 +17,7 @@ async def main():
     
     for contato in range(int(len(planilha))):
         nome = planilha.iloc[contato][0]
-        mensagem = 'Olá, {}'.format(nome)
+        mensagem = 'Bom dia, {}! Como esta?\n \nSou o *, analista de RH da *. O motivo do meu contato é referente ao seu interesse pela vaga de Agente de Negócios.\n \nComo primeira etapa do processo seletivo, há uma Avaliação de Conhecimentos Gerais e Tecnologias, que deverá ser realizada Online.\n \nSegue o link da avaliação: https://forms.gle/okysgNL59BAgxxyi6\n \nA avaliação de conhecimentos ficará disponível até segunda-feira, 30/05/2022 às 09h00.\n \nSe ficou alguma dúvida, entre em contato comigo por aqui mesmo.\n \nEspero te encontrar em breve!'.format(nome)
         numero = planilha.iloc[contato][1]
         link = 'https://api.whatsapp.com/send?phone=55{}&text={}'.format(numero, mensagem)
         print(link)
