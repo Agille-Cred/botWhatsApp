d403ae90c496b5d0ba1c76f9ff2a89ef24631e60
Renato C. Francisco <56325092+renatocfrancisco@users.noreply.github.com>
2022-06-01 16:23:57 -0300

    perf: :adhesive_bandage: v1.2.1 - tratando exceções, gitignore, readme, .exe

--- README.md
+++ README.md
@@ -6,9 +6,13 @@
 
 Para executar: **`.xlsx`** com nomes e contatos.
 Precisará entrar no WhatsApp Web com o QRCode quando solicitado.
-
 ```
 pip install pandas pyppeteer pysimplegui openpyxl
 ```
 
-Qualquer alteração/pull request no projeto é bem-vinda.
+Para criar um executável (.exe), use o [**auto-py-to-exe**](https://github.com/brentvollebregt/auto-py-to-exe) ou com o [**pyinstaller**](https://github.com/pyinstaller/pyinstaller), o comando:
+```
+pyinstaller --noconfirm --onefile --windowed --icon "icone.ico" --name "NomeDoExecutavel"  "botWhatsapp.py"
+```
+
+Qualquer alteração no projeto é bem-vinda.
--- botWhatsapp.py
+++ botWhatsapp.py
@@ -42,11 +42,11 @@ while True:
         local_planilha = values['-planilha-']
 
         if texto == '':
-            sg.popup("Insira uma mensagem")
+            sg.popup("Insira uma mensagem", font=font)
             continue
         
         if local_planilha == '':
-            sg.popup("Insira um arquivo de planilha")
+            sg.popup("Insira um arquivo de planilha", font=font)
             continue
              
         if texto != '' and local_planilha != '':
@@ -54,46 +54,59 @@ while True:
             try:
               planilha = import_planilha(local_planilha)
             except:
-              sg.popup("Erro ao importar planilha")
+              sg.popup("Erro ao importar planilha", font=font)
             
             # Pyppeteer
-            async def mandarMensagem(texto, planilha):
+            try:
+                          
+                async def mandarMensagem(texto, planilha):
 
-                df_numeros_invalidos = pds.DataFrame()
-                browser = await launch(headless=False)
-                page = await browser.newPage()
-                await page.goto('https://web.whatsapp.com/')
-                time.sleep(20)
+                    df_numeros_invalidos = pds.DataFrame()
+                    browser = await launch(headless=False)
+                    page = await browser.newPage()
+                    await page.goto('https://web.whatsapp.com/')
+                    time.sleep(20)
 
-                for contato in range(int(len(planilha))):
-                    nome = planilha.iloc[contato][0]
-                    mensagem = "Olá {}. {}".format(nome, texto)
-                    numero = planilha.iloc[contato][1]
-                    link = 'https://web.whatsapp.com/send?phone=55{}&text={}'.format(
-                        numero, mensagem)
-                    await page.goto(link)
-                    time.sleep(10)
+                    for contato in range(int(len(planilha))):
+                        nome = planilha.iloc[contato][0]
+                        mensagem = "Olá {}. {}".format(nome, texto)
+                        numero = planilha.iloc[contato][1]
+                        link = 'https://web.whatsapp.com/send?phone=55{}&text={}'.format(
+                            numero, mensagem)
+                        await page.goto(link)
+                        time.sleep(10)
 
-                    try:
-                        inp = await page.waitFor('._3J6wB')
-                    except:
-                        print('Numero {} é válido.'.format(numero))
-                    else:
-                        print('numero {} é inválido.'.format(numero))
-                        tam = int(len(df_numeros_invalidos))
+                        try:
+                            inp = await page.waitFor('._3J6wB')
+                        except:
+                            print('Numero {} é válido.'.format(numero))
+                        else:
+                            print('numero {} é inválido.'.format(numero))
+                            tam = int(len(df_numeros_invalidos))
 
-                        df_numeros_invalidos.loc[tam + 1, 'Nome'] = nome
-                        df_numeros_invalidos.loc[tam + 1, 'Contato'] = numero
-                        continue
+                            df_numeros_invalidos.loc[tam + 1, 'Nome'] = nome
+                            df_numeros_invalidos.loc[tam + 1, 'Contato'] = numero
+                            continue
+                        
+                        try:
+                            await page.click("span[data-testid='send']")
+                        except:
+                            df_numeros_invalidos.loc[tam + 1, 'Nome'] = nome
+                            df_numeros_invalidos.loc[tam + 1, 'Contato'] = numero
+                            continue
+                        else:
+                            print('Mensagem enviada a {} no número {}'.format(nome, numero))
+                        
+                        
+                        time.sleep(2)
 
-                    await page.click("span[data-testid='send']")
-                    print('Mensagem enviada a {} no número {}'.format(nome, numero))
-                    time.sleep(2)
+                    await browser.close()
+                    output(df_numeros_invalidos)
+                    sg.popup("Mensagens enviadas", font=font)
 
-                await browser.close()
-                output(df_numeros_invalidos)
-                sg.popup("Mensagens enviadas")
+                asyncio.run(mandarMensagem(texto, planilha))
+            except Exception as e:
+                sg.popup("{}\nErro ao mandar mensagens. Tentar novamente.".format(e), font=font)
 
-            asyncio.get_event_loop().run_until_complete(mandarMensagem(texto, planilha))
 
 window.close()
